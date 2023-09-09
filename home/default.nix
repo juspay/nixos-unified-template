e3d513437580bd7394d25b01b8655617a84789f7
Cameron Smith <cameron.ray.smith@gmail.com>
2023-09-08 21:18:27 -0400

    fix: update relative path to neovim in comment

--- home/default.nix
+++ home/default.nix
@@ -4,7 +4,7 @@
     homeModules = {
       default = {
         imports = [
-          # This loads ./home/neovim/default.nix - neovim configured for Haskell dev, and other things.
+          # This loads ./neovim/default.nix - neovim configured for Haskell dev, and other things.
           ./neovim
           ./starship.nix
           ./terminal.nix
