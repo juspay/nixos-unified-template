99f304a6512f59194932b2010af5e270efdfebe8
Sridhar Ratnakumar <srid@srid.ca>
2024-01-08 23:02:33 +0530

    Pin nixpkgs registry entry
    Resolves #8

--- home/default.nix
+++ home/default.nix
@@ -1,3 +1,4 @@
+{ flake, ... }:
 {
   imports = [
     # This loads ./neovim/default.nix - neovim configured for Haskell dev, and other things.
@@ -6,4 +7,9 @@
     ./terminal.nix
     # Add more of your home-manager modules here.
   ];
+
+  # Recommended Nix settings
+  nix = {
+    registry.nixpkgs.flake = flake.inputs.nixpkgs; # https://yusef.napora.org/blog/pinning-nixpkgs-flake/
+  };
 }
