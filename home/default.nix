ba8540a90c268c61f672b74e1b5c2c992c4c70b3
Sridhar Ratnakumar <srid@srid.ca>
2024-01-19 14:48:44 +0530

    Add nixPath placeholder
    Can only use after this PR is merged,
    
    https://github.com/nix-community/home-manager/pull/4031

--- home/default.nix
+++ home/default.nix
@@ -11,5 +11,9 @@
   # Recommended Nix settings
   nix = {
     registry.nixpkgs.flake = flake.inputs.nixpkgs; # https://yusef.napora.org/blog/pinning-nixpkgs-flake/
+
+    # FIXME: Waiting for this to be merged:
+    # https://github.com/nix-community/home-manager/pull/4031
+    # nixPath = [ "nixpkgs=${flake.inputs.nixpkgs}" ]; # Enables use of `nix-shell -p ...` etc
   };
 }
