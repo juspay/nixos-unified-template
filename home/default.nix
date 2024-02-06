a85bcc6e872bcd8b9aef1c2787ca59c02ead23d6
shivaraj-bh <sbh69840@gmail.com>
2024-02-06 09:33:35 +0530

    nix-gc: add service
    https://github.com/nix-community/home-manager/pull/4973

--- home/default.nix
+++ home/default.nix
@@ -11,6 +11,8 @@
   # Recommended Nix settings
   nix = {
     registry.nixpkgs.flake = flake.inputs.nixpkgs; # https://yusef.napora.org/blog/pinning-nixpkgs-flake/
+    # Garbage collect automatically every week
+    gc.automatic = true;
 
     # FIXME: Waiting for this to be merged:
     # https://github.com/nix-community/home-manager/pull/4031
