31603d418ea6bed7f7edbdc68f9d6c8db84a89dc
Sridhar Ratnakumar <3998+srid@users.noreply.github.com>
2024-03-22 11:54:05 -0400

    zsh: enable autosuggestion plugin

--- home/default.nix
+++ home/default.nix
@@ -66,6 +66,7 @@
     # For macOS's default shell.
     zsh = {
       enable = true;
+      autosuggestion.enable = true;
       envExtra = ''
         # Make Nix and home-manager installed things available in PATH.
         export PATH=/run/current-system/sw/bin/:/nix/var/nix/profiles/default/bin:$HOME/.nix-profile/bin:/etc/profiles/per-user/$USER/bin:$PATH
