0032f206d579b1b3288de3b69e0b14e7f6207917
Sridhar Ratnakumar <srid@srid.ca>
2024-03-22 12:01:47 -0400

    fix

--- home/default.nix
+++ home/default.nix
@@ -67,6 +67,7 @@
     zsh = {
       enable = true;
       autosuggestion.enable = true;
+      syntaxHighlighting.enable = true;
       envExtra = ''
         # Make Nix and home-manager installed things available in PATH.
         export PATH=/run/current-system/sw/bin/:/nix/var/nix/profiles/default/bin:$HOME/.nix-profile/bin:/etc/profiles/per-user/$USER/bin:$PATH
