d7a9e9c999cd68df3cb5c2ab8ee0e0bd15704034
Sridhar Ratnakumar <3998+srid@users.noreply.github.com>
2024-05-28 14:56:23 -0400

    direnv: Make less verbose

--- home/default.nix
+++ home/default.nix
@@ -107,6 +107,10 @@
     direnv = {
       enable = true;
       nix-direnv.enable = true;
+      config.global = {
+        # Make direnv messages less verbose
+        hide_env_diff = true;
+      };
     };
 
     # https://nixos.asia/en/git
