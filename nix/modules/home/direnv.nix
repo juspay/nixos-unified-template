95d33c23cc0a9b875c8c63fb317bc5ddb6228935
Sridhar Ratnakumar <srid@srid.ca>
2024-08-28 13:04:14 -0400

    direnv: Override nix package
    For #68

--- nix/modules/home/direnv.nix
+++ nix/modules/home/direnv.nix
@@ -1,9 +1,13 @@
-{ ... }:
+{ config, pkgs, ... }:
 {
   # https://nixos.asia/en/direnv
   programs.direnv = {
     enable = true;
-    nix-direnv.enable = true;
+    nix-direnv = {
+      enable = true;
+      # Remove after https://github.com/juspay/nix-dev-home/issues/68
+      package = pkgs.nix-direnv.override { nix = config.nix.package; };
+    };
     config.global = {
       # Make direnv messages less verbose
       hide_env_diff = true;
