1a67e4119998e6aad4f3246d22b2c7b00bb922b0
Sridhar Ratnakumar <srid@srid.ca>
2024-09-25 23:20:48 -0400

    Uncomment git name/email
    Because we now use `om init`

--- nix/modules/home/git.nix
+++ nix/modules/home/git.nix
@@ -9,8 +9,8 @@
   programs = {
     git = {
       enable = true;
-      # userName = "John Doe";
-      # userEmail = "johndoe@example.com";
+      userName = "John Doe";
+      userEmail = "johndoe@example.com";
       ignores = [ "*~" "*.swp" ];
       aliases = {
         ci = "commit";
