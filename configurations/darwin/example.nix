736edd921a81f98f0183cd875ecfd63063e817c3
Sridhar Ratnakumar <3998+srid@users.noreply.github.com>
2025-01-06 11:39:39 -0500

    darwin/hm: Automatically move old dotfiles out of the way (#119)
    It makes sense to do this because often people already have old dotfiles
    in the way.
    
    For https://github.com/juspay/nixone/issues/6

--- configurations/darwin/example.nix
+++ configurations/darwin/example.nix
@@ -18,9 +18,18 @@ in
   # https://github.com/nix-community/home-manager/issues/4026#issuecomment-1565487545
   users.users."runner".home = "/Users/runner";
 
-  # Enable home-manager for "runner" user
-  home-manager.users."runner" = {
-    imports = [ (self + /configurations/home/runner.nix) ];
+  home-manager = {
+    # Automatically move old dotfiles out of the way
+    #
+    # Note that home-manager is not very smart, if this backup file already exists it 
+    # will complain "Existing file .. would be clobbered by backing up". To mitigate this,
+    # we try to use as unique a backup file extension as possible.
+    backupFileExtension = "nixos-unified-template-backup";
+
+    # Enable home-manager for "runner" user
+    users."runner" = {
+      imports = [ (self + /configurations/home/runner.nix) ];
+    };
   };
 
   # Used for backwards compatibility, please read the changelog before changing.
