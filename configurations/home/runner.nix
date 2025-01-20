e617266beb8275551560c636bde720e17917e24b
shivaraj-bh <me@shivaraj-bh.in>
2025-01-20 20:48:23 +0530

    home-manager: Allow changing `nix` version via `nixpkgs` revision update

--- configurations/home/runner.nix
+++ configurations/home/runner.nix
@@ -7,6 +7,10 @@ in
   imports = [
     self.homeModules.default
   ];
+  # To use the `nix` from `inputs.nixpkgs` on templates using the standalone `home-manager` template
+  home.packages = [
+    config.nix.package
+  ];
   home.username = "runner";
   home.homeDirectory = lib.mkDefault "/${if pkgs.stdenv.isDarwin then "Users" else "home"}/runner";
   home.stateVersion = "24.11";
