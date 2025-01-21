d40e451a493265d61791a11e0d74503e52a34b54
Shivaraj B H <me@shivaraj-bh.in>
2025-01-21 22:46:04 +0530

    home-manager: Allow changing `nix` version via `nixpkgs` revision update (#123)
    Tested standalone `home-manager` template on:
    - [x] Ubuntu

--- configurations/home/runner.nix
+++ configurations/home/runner.nix
@@ -1,4 +1,4 @@
-{ flake, pkgs, lib, ... }:
+{ flake, pkgs, lib, config, ... }:
 let
   inherit (flake) inputs;
   inherit (inputs) self;
@@ -7,6 +7,16 @@ in
   imports = [
     self.homeModules.default
   ];
+
+  # To use the `nix` from `inputs.nixpkgs` on templates using the standalone `home-manager` template
+
+  # `nix.package` is already set if on `NixOS` or `nix-darwin`.
+  # TODO: Avoid setting `nix.package` in two places. Does https://github.com/juspay/nixos-unified-template/issues/93 help here?
+  nix.package = lib.mkDefault pkgs.nix;
+  home.packages = [
+    config.nix.package
+  ];
+
   home.username = "runner";
   home.homeDirectory = lib.mkDefault "/${if pkgs.stdenv.isDarwin then "Users" else "home"}/runner";
   home.stateVersion = "24.11";
