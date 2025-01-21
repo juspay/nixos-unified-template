af5167c6345aab08727d3cda287e70d3929f8c82
shivaraj-bh <me@shivaraj-bh.in>
2025-01-21 16:39:34 +0530

    `nix.package`

--- configurations/home/runner.nix
+++ configurations/home/runner.nix
@@ -7,10 +7,15 @@ in
   imports = [
     self.homeModules.default
   ];
+
   # To use the `nix` from `inputs.nixpkgs` on templates using the standalone `home-manager` template
+
+  # `nix.package` is already set if on `NixOS` or `nix-darwin`.
+  nix.package = lib.mkForce pkgs.nix;
   home.packages = [
     config.nix.package
   ];
+
   home.username = "runner";
   home.homeDirectory = lib.mkDefault "/${if pkgs.stdenv.isDarwin then "Users" else "home"}/runner";
   home.stateVersion = "24.11";
