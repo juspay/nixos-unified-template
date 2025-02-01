251057924139aa509eeea116e97239204d388b4a
Sridhar Ratnakumar <srid@srid.ca>
2025-02-01 08:41:01 -0500

    add desc

--- modules/flake-parts/config.nix
+++ modules/flake-parts/config.nix
@@ -8,14 +8,17 @@
           username = lib.mkOption {
             type = lib.types.str;
             default = "runner";
+            description = "Your username as shown by `id -un`";
           };
           fullname = lib.mkOption {
             type = lib.types.str;
             default = "John Doe";
+            description = "Your full name for use in Git config";
           };
           email = lib.mkOption {
             type = lib.types.str;
             default = "johndoe@example.com";
+            description = "Your email for use in Git config";
           };
         };
       };
