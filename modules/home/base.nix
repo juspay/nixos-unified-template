ba489db7eb0b3ad853cae39ff686233f8f9d2031
Sridhar Ratnakumar <srid@srid.ca>
2025-03-17 19:39:46 -0400

    Belongs here

--- modules/home/base.nix
+++ modules/home/base.nix
@@ -3,6 +3,5 @@
   imports = [
     ../../users/module/home.nix
   ];
-  home.homeDirectory = lib.mkDefault "/${if pkgs.stdenv.isDarwin then "Users" else "home"}/${config.home.username}";
   home.stateVersion = "24.11";
 }
--- users/module/home.nix
+++ users/module/home.nix
@@ -1,10 +1,11 @@
 # The home-manager module defining per-user configuration and implementation
-{ config, ... }:
+{ config, pkgs, lib, ... }:
 {
   imports = [
     ./user.nix
   ];
   config = {
     home.username = config.me.username;
+    home.homeDirectory = lib.mkDefault "/${if pkgs.stdenv.isDarwin then "Users" else "home"}/${config.me.username}";
   };
 }
