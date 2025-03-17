dbbe99d1be0608d6105fb2b72b04c75b0a12f73f
Sridhar Ratnakumar <srid@srid.ca>
2025-03-17 16:08:47 -0400

    refactor: homeDirectory can be defined in terms of username

--- configurations/home/runner/default.nix
+++ configurations/home/runner/default.nix
@@ -1,4 +1,4 @@
-{ flake, pkgs, lib, ... }:
+{ config, flake, pkgs, lib, ... }:
 let
   inherit (flake) inputs;
   inherit (inputs) self;
@@ -10,6 +10,6 @@ in
   ];
 
   home.username = me.username;
-  home.homeDirectory = lib.mkDefault "/${if pkgs.stdenv.isDarwin then "Users" else "home"}/${me.username}";
+  home.homeDirectory = lib.mkDefault "/${if pkgs.stdenv.isDarwin then "Users" else "home"}/${config.home.username}";
   home.stateVersion = "24.11";
 }
