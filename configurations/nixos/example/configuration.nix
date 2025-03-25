756ce47e6699d3ed7a126514205bc0c54499c1b9
Sridhar Ratnakumar <srid@srid.ca>
2025-03-25 19:00:25 -0400

    nixos: add disko

--- configurations/nixos/example/configuration.nix
+++ configurations/nixos/example/configuration.nix
@@ -1,7 +1,16 @@
 # NOTE: We expect this file to be supplanted by the original /etc/nixos/configuration.nix
+{ flake, ... }:
 {
+  imports = [
+    flake.inputs.disko.nixosModules.disko
+  ];
+
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
+  };
+
   # These are normally in hardware-configuration.nix
-  boot.loader.grub.device = "nodev";
   fileSystems."/" = { device = "/dev/disk/by-label/nixos"; fsType = "btrfs"; };
 
   nixpkgs.hostPlatform = "x86_64-linux";
