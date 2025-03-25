9506d7d1b09a2978a27bd7c1eadbf355f4fef10d
Sridhar Ratnakumar <srid@srid.ca>
2025-03-25 19:33:16 -0400

    sep out

--- configurations/nixos/example/configuration.nix
+++ configurations/nixos/example/configuration.nix
@@ -1,15 +1,7 @@
-# NOTE: We expect this file to be supplanted by the original /etc/nixos/configuration.nix
-{ flake, ... }:
 {
   imports = [
-    flake.inputs.disko.nixosModules.disko
-    ./disk-config.nix
+    ./configuration.nix
   ];
-
-  boot.loader = {
-    systemd-boot.enable = true;
-    efi.canTouchEfiVariables = true;
-  };
 
   nixpkgs.hostPlatform = "x86_64-linux";
   networking.hostName = "example";
