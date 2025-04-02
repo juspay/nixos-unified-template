4b336bb306d2979c32e8992f5293f9614c931d14
Shivaraj B H <me@shivaraj-bh.in>
2025-04-02 19:40:02 +0530

    quote to pass shellcheck

--- modules/flake/activate-home.nix
+++ modules/flake/activate-home.nix
@@ -7,7 +7,7 @@
         name = "activate-home";
         text = ''
           set -x
-          ${lib.getExe self'.packages.activate} $(id -un)@
+          ${lib.getExe self'.packages.activate} "$(id -un)"@
         '';
       };
     };
