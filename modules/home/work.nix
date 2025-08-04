d4ca489b6d2eeff47e60ac9aac3f1764ecd8d642
Nikhil Singh <nik.singh710@gmail.com>
2025-08-04 17:14:43 +0530

    Update modules/home/work.nix
    Co-authored-by: Shivaraj B H <me@shivaraj-bh.in>

--- modules/home/work.nix
+++ modules/home/work.nix
@@ -1,8 +1,8 @@
 let
   initContent = # sh
     ''
-      # Incraesing stack size for haskell project compilation
-      # see: <https://github.com/juspay/nixone/issues/34>
+      # Workaround for linker errors like https://github.com/nammayatri/nammayatri/blob/49b26c595681b68536f0357884c82766047805b1/Backend/README.md?plain=1#L97-L103
+      # see also: <https://github.com/juspay/nixone/issues/34>
       ulimit -s 65500
     '';
 in
