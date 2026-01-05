046fb856cb0602c532751c1a5b560efa33259fa8
vg006 <devvg006@gmail.com>
2026-01-05 09:10:12 +0530

    fix: Update packages and field in the flake

--- assets/flake.nix
+++ assets/flake.nix
@@ -1,10 +1,10 @@
 {
-  description = "nixos-unified-template flake";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
-  outputs = inputs@{ flake-parts, nixpkgs, ... }:
+  outputs =
+    inputs@{ flake-parts, nixpkgs, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [
         "x86_64-linux"
@@ -12,22 +12,17 @@
         "x86_64-darwin"
         "aarch64-darwin"
       ];
-      perSystem = { system, ... }:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-        in {
+      perSystem =
+        { pkgs, ... }:
+        {
           devShells.default = pkgs.mkShell {
             packages = with pkgs; [
-              # Packages used to take demo
               vhs
               ffmpeg
               ttyd
               chromium
-
-              # Packages used within demo
-              tree
-              # xclip
-              # wl-clipboard
+              eza
+              nerd-fonts.jetbrains-mono
             ];
           };
         };
