c45c7836014d7d314edf1b2fba2bef51e03459c7
Vijay Ganesh <144366222+vg006@users.noreply.github.com>
2026-02-05 02:58:15 +0530

    docs: Add demo GIF to the README (#214)
    This PR fixes the issue #73 by adding the demo as GIF to the
    `README.md`. This demo is taken using
    [`vhs`](https://github.com/charmbracelet/vhs?tab=readme-ov-file#vhs).
    
    To produce the demo, [install
    `vhs`](https://github.com/charmbracelet/vhs?tab=readme-ov-file#installation)
    and execute the following command under the `assets` directory.
    ```fish
    vhs demo.tape
    ```
    which will produce the demo in the `.gif` and `.mp4` formats
    
    ## Changes introduced
    
    - `assets` directory
    - `demo.tape`: Declarative configuration of demo for
    [`vhs`](https://github.com/charmbracelet/vhs?tab=readme-ov-file#vhs-command-reference).
      - `out.gif`: Demo as GIF format.
      - `out.mp4`:  Demo as MP4 format.
    
    ## Result
    >
    [Preview](https://github.com/vg006/nixos-unified-template/tree/fix/docs?tab=readme-ov-file#getting-started)
    of the output in my fork
    
    ![Demo Sample](https://vhs.charm.sh/vhs-340BDTCzD9DnSkifpgyZ29.gif)
    
    ---------
    
    Signed-off-by: vg006 <devvg006@gmail.com>
    Co-authored-by: Sridhar Ratnakumar <3998+srid@users.noreply.github.com>

---
+++ assets/README.md
@@ -0,0 +1,12 @@
+# Demo
+
+## Create the demo
+
+```
+nix run
+```
+
+[VHS](https://github.com/charmbracelet/vhs) will automatically:
+- Execute all commands in the tape file
+- Capture the terminal output
+- Generate `out.gif` and `out.mp4`
---
+++ assets/flake.nix
@@ -0,0 +1,42 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+  outputs =
+    inputs@{ flake-parts, nixpkgs, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = nixpkgs.lib.systems.flakeExposed;
+      perSystem =
+        { pkgs, ... }: let
+          fontsConf = pkgs.makeFontsConf {
+            fontDirectories = [
+              pkgs.nerd-fonts.jetbrains-mono
+            ];
+          };
+          app = pkgs.writeShellApplication {
+            name = "demo";
+            runtimeEnv = {
+              output_dir = "nixconfig";
+              FONTCONFIG_FILE = "${fontsConf}";
+            };
+            runtimeInputs = with pkgs; [
+              omnix
+              vhs
+              eza
+              fish
+              fontconfig
+            ];
+            text = ''
+              nix flake prefetch github:juspay/nixos-unified-template
+              trap 'rm -rf ./$output_dir' EXIT; vhs ./demo.tape
+            '';
+          };
+        in
+        {
+          apps.default = {
+            program = "${app}/bin/demo";
+          };
+        };
+    };
+}
